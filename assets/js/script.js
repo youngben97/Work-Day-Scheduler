300b76913d145b9439def17017e127b4fa13585e
GreatGaspargoo <b.c.young.97@gmail.com>
2023-08-11 17:34:47 -0600

    added dayjs for date display

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,4 +20,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var today = dayjs();
+  $('#currentDay').text(today.format('MMM D, YYYY'));
 });
