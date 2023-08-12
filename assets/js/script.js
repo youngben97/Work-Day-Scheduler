d77cee540595638c31a10ef2421f6a0106af3a11
GreatGaspargoo <b.c.young.97@gmail.com>
2023-08-11 18:17:37 -0600

    added listener for click event to get id of time-block

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,13 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  var saveBtnEl = $('.saveBtn');
+  saveBtnEl.on('click', function () {
+    var timeBlock = $(this).parent().attr('id');
+    console.log(timeBlock);
+    return timeBlock;
+  })
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
